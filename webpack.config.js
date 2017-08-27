aaa00f90d1196a14a02efcdaac4ee4bdcfde4737
ashwinkrishna <ashwin.krishna@razorthink.net>
2017-08-27 18:47:35 +0530

    Webpack watch mode added

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');//Html creation plugin
+const CleanWebpackPlugin = require('clean-webpack-plugin');//clean up dist
 
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management',
       filename: 'assets/index.html'
